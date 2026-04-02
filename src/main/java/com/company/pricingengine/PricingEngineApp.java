e99e07723e0b40d11686aa3d65f0028802a01a0e
CarlosCruz-117 <ccdeh@hotmail.com>
2026-04-02 21:17:19 +0200

    • Estructura base de proyecto (sujeto a cambios) • pom.xml base (sujeto a cambios) • API definida con anotaciones Swagger (Controller sin impl) • TDD -> Test de integración de API (fallando) • Parametrización de CTs vía CSV

---
+++ src/main/java/com/company/pricingengine/PricingEngineApp.java
@@ -0,0 +1,12 @@
+package com.company.pricingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingEngineApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingEngineApp.class, args);
+    }
+}
